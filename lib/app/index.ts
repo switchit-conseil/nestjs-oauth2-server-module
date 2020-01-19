b05e69a1f8bcc4edf6622366532ff8569ff51695
Nicolas MACHEREY <n.macherey@wakeonweb.com>
2020-01-19 08:40:09 +0100

    Added missing index files

---
+++ lib/app/index.ts
@@ -0,0 +1,6 @@
+export * from './command';
+export * from './event';
+export * from './interfaces';
+export * from './oauth2.module';
+export * from './oauth2-core.module';
+export * from './oauth2.constants';
---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+export * from './app';
+export * from './domain';
+export * from './infrastructure';
+export * from './ui';
